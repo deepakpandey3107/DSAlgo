6b674f841edd73fd058d681a93c946d768a98891
Soma Deepak <somadeepak@Somas-MacBook-Pro-4.local>
2016-12-12 22:08:14 +0530

    cleaned up client

--- src/main/java/com/pandey/graph/directed/problems/DigraphClient.java
+++ src/main/java/com/pandey/graph/directed/problems/DigraphClient.java
@@ -1,25 +1,37 @@
 package com.pandey.graph.directed.problems;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 import java.util.Stack;
 
 public class DigraphClient {
 
+	/**
+	 * Sample Input 
+	5 6
+	12 25
+	0 4
+	0 1 5 0
+	0 2 2 1
+	0 3 5 0
+	1 4 5 0
+	2 4 2 1
+	3 4 5 0
+	 * 
+	 */
+	
 	public static void main(String[] args) {
-		Integer V = 5; Integer s = 0; Integer v= 4 ;
-		Double cycleSpeed = 12d; Double bikeSpeed = 25d;
-		EdgeWeightedDigraph graph = new EdgeWeightedDigraph(V);
-		addEdge(graph, 0, 1, 5d, 0, cycleSpeed, bikeSpeed);
-		addEdge(graph, 0, 2, 2d, 1, cycleSpeed, bikeSpeed);
-		addEdge(graph, 0, 3, 5d, 0, cycleSpeed, bikeSpeed);
-		addEdge(graph, 1, 4, 5d, 0, cycleSpeed, bikeSpeed);
-		addEdge(graph, 2, 4, 2d, 1, cycleSpeed, bikeSpeed);
-		addEdge(graph, 3, 4, 5d, 0, cycleSpeed, bikeSpeed);
 		
-		DigraphProcessor processor = new DigraphProcessor(graph, s);
-		System.out.println("hasPath:" +processor.hasPathTo(v));
-		System.out.println("time needed:" +processor.timeTo(v));
+		GraphProcRequest graphProcReq = readInput();
+		EdgeWeightedDigraph graph = new EdgeWeightedDigraph(graphProcReq.V());
+		addEdges(graphProcReq, graph);
 
-		Stack<DirectedEdge> path = processor.path(v);
+		DigraphProcessor processor = new DigraphProcessor(graph, graphProcReq.S());
+		System.out.println("hasPath:" +processor.hasPathTo(graphProcReq.D()));
+		System.out.println("time needed:" +processor.timeTo(graphProcReq.D()));
+
+		Stack<DirectedEdge> path = processor.path(graphProcReq.D());
 		if(path != null){
 			while(!path.isEmpty()){
 			System.out.println(path.pop().toString());
@@ -27,14 +39,110 @@ public class DigraphClient {
 		}
 		
 	}
-	
-	private static void addEdge(EdgeWeightedDigraph graph, Integer from, Integer to, Double distance, 
-						Integer both, Double cycleSpeed, Double bikeSpeed){
-		DirectedEdge cycleEdge = new DirectedEdge(from, to, distance, new Vehicle(cycleSpeed, VehicleType.BICYCLE) );
-		graph.addEdge(cycleEdge);
-		if(both==0){
-			DirectedEdge bikeEdge = new DirectedEdge(from, to, distance, new Vehicle(bikeSpeed, VehicleType.BIKE) );
-			graph.addEdge(bikeEdge);
+
+	private static void addEdges(GraphProcRequest graphProcReq, EdgeWeightedDigraph graph) {
+		for(EdgeRequest edReq : graphProcReq.getEdges()){
+			addEdge(graph, edReq.getFrom(), edReq.getTo(), edReq.getDist(), VehicleType.BICYCLE);
+			if(edReq.getBoth() == 0)
+			addEdge(graph, edReq.getFrom(), edReq.getTo(), edReq.getDist(), VehicleType.BIKE);
 		}
 	}
+	
+	private static void addEdge(EdgeWeightedDigraph graph, Integer from, Integer to, Double dist, VehicleType type) {
+		DirectedEdge edge = new DirectedEdge(from, to, dist, new Vehicle(type.getSpeed(), type) );
+		graph.addEdge(edge);
+	}
+
+	private static GraphProcRequest readInput(){
+		
+		Scanner sc = new Scanner(System.in);
+		Integer n = sc.nextInt() ; 		Integer m = sc.nextInt() ;
+		Double v1 = sc.nextDouble();	Double v2 = sc.nextDouble();
+		Integer x = sc.nextInt(); 		Integer y = sc.nextInt();
+		GraphProcRequest graphProcReq = new GraphProcRequest(n,x,y,v1,v2);
+		VehicleType.BICYCLE.setSpeed(graphProcReq.getCycleSpeed());;
+		VehicleType.BIKE.setSpeed(graphProcReq.getBikeSpeed());		
+		for (int i = 0; i < m; i++ ){
+			Integer from = sc.nextInt();
+			Integer to =sc.nextInt();
+			Double dist = sc.nextDouble();
+			Integer both = sc.nextInt();
+			EdgeRequest request = new EdgeRequest(from,to,dist,both);
+			request.dist = dist;
+			graphProcReq.add(request);
+		}
+		sc.close();
+		return graphProcReq;
+	}
+	
+	
+	private static class EdgeRequest{
+		private Integer from,to,both;
+		private Double dist;
+		public EdgeRequest(Integer from, Integer to, Double dist, Integer both) {
+			this.from = from;
+			this.to = to;
+			this.dist = dist;
+			this.both = both;
+		}
+		
+		public Integer getFrom() {
+			return from;
+		}
+		
+		public Integer getTo() {
+			return to;
+		}
+		
+		public Double getDist() {
+			return dist;
+		}
+		
+		public Integer getBoth() {
+			return both;
+		}
+		
+	}
+	
+	private static class GraphProcRequest{
+		private Integer V, s, d;
+		private List<EdgeRequest> edges;
+		private Double cycleSpeed, bikeSpeed;
+		public GraphProcRequest(Integer v, Integer s, Integer d, Double cycleSpeed, Double bikeSpeed) {
+			this.V = v;
+			this.s = s;
+			this.d = d;
+			this.cycleSpeed = cycleSpeed;
+			this.bikeSpeed = bikeSpeed;
+			this.edges = new ArrayList<>();
+		}
+		public void add(EdgeRequest edgeRequest) {
+			this.edges.add(edgeRequest);
+		}
+		
+		public Integer V() {
+			return V;
+		}
+		
+		public Integer S() {
+			return s;
+		}
+		
+		public Integer D() {
+			return d;
+		}
+		
+		public List<EdgeRequest> getEdges() {
+			return edges;
+		}
+		
+		public Double getCycleSpeed() {
+			return cycleSpeed;
+		}
+		
+		public Double getBikeSpeed() {
+			return bikeSpeed;
+		}
+	}
+	
 }
--- src/main/java/com/pandey/graph/directed/problems/VehicleType.java
+++ src/main/java/com/pandey/graph/directed/problems/VehicleType.java
@@ -2,4 +2,12 @@ package com.pandey.graph.directed.problems;
 
 public enum VehicleType {
 	BIKE,BICYCLE;
+	private Double speed;
+	public void setSpeed(Double speed) {
+		this.speed = speed;
+	}
+	
+	public Double getSpeed() {
+		return speed;
+	}
 }
